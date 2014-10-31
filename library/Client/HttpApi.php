1a30015a55e6dd18a6178a7684600c25525ad1fe
Leonardo Grasso <me@leonardograsso.com>
2014-10-31 17:05:06 +0100

    HttpApi

--- library/Client/HttpApi.php
+++ library/Client/HttpApi.php
@@ -8,9 +8,311 @@
  */
 namespace Matryoshka\Service\Api\Client;
 
+use Matryoshka\Service\Api\Exception;
+use Matryoshka\Service\Api\Profiler\ProfilerAwareInterface;
+use Matryoshka\Service\Api\Profiler\ProfilerAwareTrait;
+use Matryoshka\Service\Api\Response\Decoder\DecoderInterface;
+use Matryoshka\Service\Api\Response\Decoder\Hal;
+use Zend\Http\Request;
+use Zend\Http\Client;
+use Zend\Json\Json;
+use Zend\Http\Header\ContentType;
+use Zend\Http\Response;
 /**
  * Class HttpApi
  */
-class HttpApi
+class HttpApi implements ProfilerAwareInterface
 {
+    use ProfilerAwareTrait;
+
+    const FORMAT_JSON = 'json';
+    const FORMAT_XML  = 'xml';
+
+    /**
+     * @var Client
+     */
+    protected $httpClient;
+
+    /**
+     * @var Request
+     */
+    protected $baseRequest;
+
+    /**
+     * @var DecoderInterface
+     */
+    protected $responseDecoder;
+
+    /**
+     * @var array
+     */
+    protected $validStatusCodes = [];
+
+    /**
+     * @var string
+    */
+    protected $requestFormat = self::FORMAT_JSON;
+
+    /**
+     * @var Request
+     */
+    protected $lastRequest = null;
+
+    /**
+     * @var Response
+     */
+    protected $lastResponse = null;
+
+    /**
+     * @param Client $httpClient
+     * @param Request $baseRequest
+     */
+    public function __construct(Client $httpClient = null, Request $baseRequest = null)
+    {
+        $this->httpClient = $httpClient ? $httpClient : new Client();
+        $this->baseRequest = $baseRequest ? $baseRequest : $this->httpClient->getRequest();
+    }
+
+
+    /**
+     * @param string $method
+     * @param string $id
+     * @param array $query
+     * @param array $data
+     * @return Request
+     */
+    public function prepareRequest($method, $relativePath = null, array $data = [], array $query = [])
+    {
+        $request = $this->cloneBaseRequest();
+        $request->setMethod($method);
+        if ($relativePath) {
+            $request->getUri()->setPath($request->getUri()->getPath() . $relativePath);
+        }
+
+        $queryParams = $request->getQuery();
+        foreach ($query as $name => $value) {
+            $queryParams->set($name, $value);
+        }
+
+        if (!empty($data)) {
+            $request->setContent($this->encodeBodyRequest($data));
+        }
+
+        $request->getHeaders()->addHeaderLine('Content-Type', 'application/' . $this->getRequestFormat())
+                              ->addHeader($this->getResponseDecoder()->getAcceptHeader());
+        return $request;
+    }
+
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function dispatchRequest(Request $request)
+    {
+        if ($this->profiler) {
+            $this->getProfiler()->profilerStart();
+        }
+
+        // Send request
+        /** @var $response Response */
+        $response = $this->httpClient->dispatch($request);
+
+        if ($this->profiler) {
+            $this->getProfiler()->profilerFinish($this->httpClient);
+        }
+
+        $this->lastRequest = $request;
+        $this->lastResponse = $response;
+
+        $validStatusCodes = $this->getValidStatusCodes();
+        $responseStatusCode = $response->getStatusCode();
+        $decodedResponse = (array) $this->getResponseDecoder()->decode($response);
+
+        if ((empty($validStatusCodes) && $response->isSuccess())
+            || in_array($responseStatusCode, $validStatusCodes)) {
+            return $decodedResponse;
+        }
+
+        throw $this->getInvalidResponseException($decodedResponse, $response);
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    protected function encodeBodyRequest(array $data)
+    {
+        $requestFormat = $this->getRequestFormat();
+
+        switch ($requestFormat) {
+            case self::FORMAT_JSON:
+                $bodyRequest = Json::encode($data);
+                break;
+            case self::FORMAT_XML:
+
+                // TODO: not yet implemented
+                // break;
+            default:
+                throw new Exception\InvalidFormatException(sprintf(
+                'The "%s" format is invalid or not supported',
+                $requestFormat
+                ));
+                break;
+        }
+
+        return $bodyRequest;
+    }
+
+    /**
+     * @param $bodyDecodeResponse
+     * @return Exception\InvalidResponseException
+     */
+    protected function getInvalidResponseException(array $bodyDecodeResponse, Response $response)
+    {
+        $contentType = $response->getHeaders()->get('Content-Type');
+
+        if ($contentType instanceof ContentType && $contentType->match('application/problem+*')) {
+
+            $apiProblemDefaults = [
+                'type'      => $response->getReasonPhrase(),
+                'title'     => '',
+                'status'    => $response->getStatusCode(),
+                'detail'    => '',
+                'instance'  => '',
+            ];
+
+            $bodyDecodeResponse += $apiProblemDefaults;
+
+            //Setup remote exception
+            $remoteExceptionStack = isset($bodyDecodeResponse['exception_stack']) && is_array($bodyDecodeResponse['exception_stack']) ?
+            $bodyDecodeResponse['exception_stack'] : [];
+
+            array_unshift($remoteExceptionStack, [
+                'message' => $bodyDecodeResponse['detail'],
+                'code'    => $bodyDecodeResponse['status'],
+                'trace'   => isset($bodyDecodeResponse['trace']) ? $bodyDecodeResponse['trace'] : null,
+            ]);
+
+            //Setup exception
+            $exception = new Exception\ApiProblem\DomainException(
+                $bodyDecodeResponse['detail'],
+                $bodyDecodeResponse['status'],
+                Exception\RemoteException::factory($remoteExceptionStack) //Set remote ex chain as previous of current ex
+            );
+            $exception->setType($bodyDecodeResponse['type']);
+            $exception->setTitle($bodyDecodeResponse['title']);
+            foreach ($apiProblemDefaults as $key => $value) {
+                unset($bodyDecodeResponse[$key]);
+            }
+            $exception->setAdditionalDetails($bodyDecodeResponse);
+        } else {
+            $exception = new Exception\InvalidResponseException($response->getReasonPhrase(), $response->getStatusCode());
+            $exception->setResponse($response);
+        }
+
+        return $exception;
+    }
+
+
+    public function getResponseDecoder()
+    {
+        if (null === $this->responseDecoder) {
+            $this->setResponseDecoder(new Hal);
+        }
+
+        return $this->responseDecoder;
+    }
+
+    public function setResponseDecoder(DecoderInterface $decoder)
+    {
+        $this->responseDecoder = $decoder;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValidStatusCodes()
+    {
+        return $this->validStatusCodes;
+    }
+
+    /**
+     * @param array $validStatusCodes
+     * @return $this
+     */
+    public function setValidStatusCodes(array $validStatusCodes)
+    {
+        $this->validStatusCodes = $validStatusCodes;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRequestFormat()
+    {
+        return $this->requestFormat;
+    }
+
+    /**
+     * @param $requestFormat
+     * @return $this
+     */
+    public function setRequestFormat($requestFormat)
+    {
+        $this->requestFormat = $requestFormat;
+        return $this;
+    }
+
+    /**
+     * @return Request
+     */
+    public function getBaseRequest()
+    {
+        return $this->baseRequest;
+    }
+
+    /**
+     * @param Request $request
+     * @return $this
+     */
+    public function setBaseRequest(Request $request)
+    {
+        $this->baseRequest = $request;
+        return $this;
+    }
+
+    /**
+     * @return Request
+     */
+    public function cloneBaseRequest()
+    {
+        return unserialize(serialize($this->baseRequest));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLastRequest()
+    {
+        return $this->lastRequest;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLastResponse()
+    {
+        return $this->lastResponse;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLastResponseData()
+    {
+        return $this->getResponseDecoder()->getLastPayload();
+    }
 }
---
+++ tests/Client/HttpApiTest.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Matryoshka Service API
+ *
+ * @link        https://github.com/matryoshka-model/service-api
+ * @copyright   Copyright (c) 2014, Ripa Club
+ * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+namespace MatryoshkaServiceApiTest\Client;
+
+use Zend\Http\Request;
+use Zend\Http\Response;
+use Matryoshka\Service\Api\Client\HttpApi;
+
+class HttpApiTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var HttpApi
+     */
+    protected $httpApi;
+
+    /**
+     * @return array
+     */
+    public function providerServiceResponse()
+    {
+        return [
+            [['get', 'path', ['test' => 'test']], '{"test": "test"}', 'application/json', 'json'],
+            [['get', null, ['test' => 'test']], '{"test": "test"}', 'application/json', 'json'],
+            [['head', 'path', ['test' => 'test']], '{"test": "test"}', 'application/json', 'json'],
+            [['options', 'path', ['test' => 'test']], '{"test": "test"}', 'application/json',  'json'],
+            [['patch', 'path', ['test' => 'test']], '{"test": "test"}', 'application/json', 'json'],
+            [['post', 'path', ['test' => 'test'], ['test' => 'test']], '{"test": "test"}', 'application/json', 'json'],
+            [['put', 'path', ['test' => 'test'], ['test' => 'test']], '{"test": "test"}', 'application/json', 'json'],
+            [['delete', 'path', ['test' => 'test']], '{"test": "test"}', 'application/json', 'json'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function providerServiceRequestResponseException()
+    {
+        //Params: [$params, $responseContent, $responseContentType, $responseStatusCode, $format, $exceptionType]
+
+        $apiProblemResponse = '{
+    "type": "http://example.com/probs/out-of-credit",
+    "title": "You do not have enough credit.",
+    "detail": "Your current balance is 30, but that costs 50.",
+    "instance": "http://example.net/account/12345/msgs/abc",
+    "balance": 30,
+    "accounts": ["http://example.net/account/12345",
+                 "http://example.net/account/67890"]
+   }';
+
+        return [
+            //Bad Responses
+            [['get', null], '{"test": "test"}', 'application/json', 500, 'json'],
+            [['post', 'path', ['test' => 'test']], '{"test": "test"}', 'application/json', 500, 'json'],
+            [['delete', 'id'], '', 'application/json', 500, 'json'],
+            [['get', null], $apiProblemResponse, 'application/problem+json', 500, 'json', '\Matryoshka\Service\Api\Exception\ApiProblem\DomainException'],
+            [['get', 'id'], '', 'application/problem+json', 502, 'json', '\Matryoshka\Service\Api\Exception\ApiProblem\DomainException'],
+            [['get', 'id'], '', 'application/invalid-response-format', 502, 'json', '\Matryoshka\Service\Api\Exception\InvalidFormatException'],
+            [['get', 'id'], null, '', 502, 'json'], //content-type missing
+
+            //Bad requests
+            [['post', 'path', ['test' => 'test']], '', 'application/json', 502, 'invalid-request-format', '\Matryoshka\Service\Api\Exception\InvalidFormatException'],
+        ];
+    }
+
+    public function setUp()
+    {
+        $this->httpApi = new HttpApi();
+    }
+
+    public function testGetSetValidStatusCodes()
+    {
+        $this->assertSame($this->httpApi, $this->httpApi->setValidStatusCodes([200, 201]));
+        $this->assertCount(2, $this->httpApi->getValidStatusCodes());
+    }
+
+    public function testGetSetRequestFormat()
+    {
+        $this->assertSame($this->httpApi, $this->httpApi->setRequestFormat('json'));
+        $this->assertSame('json', $this->httpApi->getRequestFormat());
+    }
+
+    public function testGetSetBaseRequest()
+    {
+        $request = new Request();
+        $this->assertSame($this->httpApi, $this->httpApi->setBaseRequest($request));
+        $this->assertSame($request, $this->httpApi->getBaseRequest());
+    }
+
+    public function testGetLastRequest()
+    {
+        $this->assertNull($this->httpApi->getLastRequest());
+    }
+
+    public function testGetLastResponse()
+    {
+        $this->assertNull($this->httpApi->getLastResponse());
+    }
+
+
+    public function testGetLastResponseData()
+    {
+        $this->assertNull($this->httpApi->getLastResponseData());
+    }
+
+    public function testCloneBaseRequest()
+    {
+        $request = $this->httpApi->getBaseRequest();
+        $cloneRequest = $this->httpApi->cloneBaseRequest();
+        $this->assertInstanceOf('Zend\Http\Request', $cloneRequest);
+        $this->assertNotSame($request, $cloneRequest);
+    }
+
+    /**
+     * @param $contentResponse
+     * @dataProvider providerServiceResponse
+     */
+    public function testHttpMethod(array $params, $contentResponse, $responseContentType, $typeResponse)
+    {
+        $httpClient = $this->getMockBuilder('Zend\Http\Client')
+            ->disableOriginalConstructor()
+            ->setMethods(['dispatch', 'getResponse'])
+            ->getMock();
+
+        $expectedResponse = new Response();
+        $expectedResponse->setContent($contentResponse);
+        $expectedResponse->getHeaders()->addHeaderLine('Content-Type', $responseContentType);
+
+        $httpClient->expects($this->any())
+            ->method('dispatch')
+            ->will($this->returnValue($expectedResponse));
+
+        $httpClient->expects($this->any())
+            ->method('getResponse')
+            ->will($this->returnValue($expectedResponse));
+
+        $api = new HttpApi($httpClient);
+        $profiler = $this->getMock('Matryoshka\Service\Api\Profiler\ProfilerInterface');
+
+        $api->setRequestFormat($typeResponse);
+        $api->setProfiler($profiler);
+
+        $request = call_user_func_array([$api, 'prepareRequest'], $params);
+        $this->assertInstanceOf('\Zend\Http\Request', $request);
+        $this->assertEquals($api->getBaseRequest()->getUri()->getPath() . (string) $params[1], $request->getUri()->getPath());
+
+        $response = $api->dispatchRequest($request);
+        $this->assertSame($api->getResponseDecoder()->decode($expectedResponse), $response);
+
+
+        $this->assertSame($request, $api->getLastRequest());
+        $this->assertSame($expectedResponse, $api->getLastResponse());
+        $this->assertSame($api->getResponseDecoder()->getLastPayload(), $api->getLastResponseData());
+    }
+
+
+    /**
+     * @param array $params
+     * @param string $responseContent
+     * @param string $format
+     * @param string $exceptionType
+     * @dataProvider providerServiceRequestResponseException
+     */
+    public function testHttpMethodRequestResponseException(
+        array $params,
+        $responseContent,
+        $responseContentType,
+        $responseStatusCode,
+        $format,
+        $exceptionType = '\Matryoshka\Service\Api\Exception\InvalidResponseException')
+    {
+        $httpClient = $this->getMockBuilder('Zend\Http\Client')
+            ->disableOriginalConstructor()
+            ->setMethods(['dispatch', 'getResponse'])
+            ->getMock();
+
+        $response = new Response();
+        $response->setContent($responseContent);
+        if ($responseContentType) {
+            $response->getHeaders()->addHeaderLine('Content-Type: ' . $responseContentType);
+        }
+        $response->setStatusCode($responseStatusCode);
+
+
+        $httpClient->expects($this->any())
+            ->method('dispatch')
+            ->will($this->returnValue($response));
+
+        $httpClient->expects($this->any())
+            ->method('getResponse')
+            ->will($this->returnValue($response));
+
+        $api = new HttpApi($httpClient);
+        $api->setRequestFormat($format);
+
+        $this->setExpectedException($exceptionType);
+        $api->dispatchRequest(call_user_func_array([$api, 'prepareRequest'], $params));
+    }
+
+
+    public function testPrepareRequestShouldThrowExceptionOnInvalidFormat()
+    {
+        $this->httpApi->setRequestFormat('invalid format');
+
+        $this->setExpectedException('\Matryoshka\Service\Api\Exception\InvalidFormatException');
+        $this->httpApi->prepareRequest('post', null, ['foo' => 'baz']);
+    }
+
+}
